e1c5e52d626b980adba1907bde81cf64bb31b6a9
vk799576 <vk799576@gmail.com>
2026-08-24 15:43:09 +0530

    Connect frontend to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://teacher-attendance-server.onrender.com",
+  baseURL: "https://teacher-attendance-server.onrender.com/api",
 });
 
 export default API;
